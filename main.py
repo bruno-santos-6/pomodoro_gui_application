99336d3db2fbd68ef37366339f661aa701881597
brunodos-santos <bruno6.santos@usp.br>
2023-07-31 16:53:23 -0300

    últimos ajustes do botão de reset

--- main.py
+++ main.py
@@ -6,14 +6,23 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
+WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
 timer = None
+
 # ---------------------------- TIMER RESET ------------------------------- # 
+
+
 def reset_timer():
     window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
@@ -36,6 +45,8 @@ def start_timer():
     else:
         count_down(work_sec)
         title_label.config(text="Work", fg=GREEN)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
 
@@ -55,6 +66,7 @@ def count_down(count):
         for _ in range(work_sessions):
             marks += "✔"
         check_marks.config(text=marks)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
